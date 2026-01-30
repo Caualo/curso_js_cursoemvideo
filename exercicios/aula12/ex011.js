23e9402114ed946e409f17f481d7339446eb3693
Cauã Ferrigolli <cauaferrigolli@gmail.com>
2026-01-30 13:16:03 -0300

    Create 3 exercices

---
+++ exercicios/aula12/ex011.js
@@ -0,0 +1,12 @@
+
+let idade = 17
+
+if (idade < 18) {
+    console.log(`Sua idade é ${idade}, Você não pode dirigir e não pode beber.`)
+
+}else if (idade >= 18 && idade <= 22 ) {
+    console.log(`Sua idade é de ${idade} anos, você pode dirigir mas não pode beber.`)
+
+}else {
+    console.log(`Sua idade é de ${idade} anos, Você pode beber e dirigir.`)
+}
